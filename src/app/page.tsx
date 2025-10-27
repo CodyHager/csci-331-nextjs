4a850569dee2daab092e4e42665362b10711cc3a
CodyHager <chager004@gmail.com>
2025-10-27 16:30:28 -0600

    init

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+import { Typography } from "@mui/material";
+
+export default function Home() {
+  return (
+    <Typography>Hello world</Typography>
+  );
+}
